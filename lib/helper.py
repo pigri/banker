8f317ca23eb5ac511e28f0727ab858ec0cc73e68
David Papp <david.papp@bitpanda.com>
2021-03-02 17:18:43 +0100

    :bug: Date parse modifing

--- lib/helper.py
+++ lib/helper.py
@@ -9,7 +9,7 @@ def dateConverter(input_date_format, date_string):
     date_format = str(input_date_format)
     formated_date = datetime.strptime(date_string, date_format).date()
     # REVOLUT DATE FIX
-    formated_date = formated_date.replace(2020)
+    # formated_date = formated_date.replace(2020)
     return formated_date
 
 def genUuid():
